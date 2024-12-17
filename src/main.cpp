bcb416550d0c5a8aec49ed5b5b602ae8f5c21360
ImNutes <150874362+ImNutes@users.noreply.github.com>
2024-12-17 21:03:28 +0000

    hi guys

--- src/main.cpp
+++ src/main.cpp
@@ -16,21 +16,27 @@
  * in any other program, i would use the fft and be done.
 */
 void dft_range(std::vector<double> input, size_t pos, size_t range,std::vector<std::complex<double>>& res) {
-    for(size_t i = pos; i < range || i < input.size(); ++i) {
+    std::cout << "Thread starting from pos " << pos << '\n';
+    size_t i = pos;
+    for(; i < range || i < input.size(); ++i) {
+        if(i == (range + pos)/4) std::cout << "25% done\n";
+        if(i == (range + pos)/2) std::cout << "50% done\n";
+        if(i == (3 * (range + pos))/2) std::cout << "75% done\n";
         for(size_t j = 0; j < input.size(); ++j) {
             std::complex<double> exponent = std::complex<double>(0.0, (-TAU * pos * j)/input.size());
             res[pos] += input[j] * std::exp(exponent);
         }
     }
+    std::cout << "Thread finished at " << i <<  '\n';
 }
 std::vector<std::complex<double>> dft_para(std::vector<double> input) {
-    const int THREAD_COUNT = 8;
-    int THREAD_SPACE = ceil(input.size()/THREAD_COUNT);
-    std::array<THREAD_COUNT> threads;
+    const size_t THREAD_COUNT = 8;
+    size_t THREAD_SPACE = ceil(input.size()/THREAD_COUNT);
+    std::array<std::thread, THREAD_COUNT> threads;
     std::vector<std::complex<double>> out(input.size());
-    for(size_t k = 0; k < THREAD_COUNT; k += THREAD_SPACE) {
-        std::cout << k << " of " << input.size() << '\n';
-        threads[k] = std::thread(dft_range, k, (THREAD_SPACE), out);
+    for(size_t k = 0; k < THREAD_COUNT; k += 1) {
+        std::cout << k+1 << " of " << THREAD_COUNT << '\n';
+        threads[k] = std::thread(dft_range, std::ref(input), k * THREAD_SPACE, THREAD_SPACE, std::ref(out));
     }
     for(auto& i : threads) {
         i.join();
@@ -38,26 +44,28 @@ std::vector<std::complex<double>> dft_para(std::vector<double> input) {
     return out;
 }
 
+//no
+std::vector<std::complex<double>> dft_seq(std::vector<double> input) {
+    std::vector<std::complex<double>> res;
+    dft_range(input, 0, input.size(), res);
+    return res;
+}
+
 int main() {
     SndfileHandle file;
     file = SndfileHandle("audio.wav");
     size_t samples = file.frames();
     std::vector<double> input(samples);
-    std::vector<float> buf(samples);
-    sf_readf_float(file, buf.data(), samples);
-    for(size_t i = 0; i < samples; ++i) {
-        input[i] = static_cast<double>(buf[i]);
-    }
-    sf_close(file);
+    file.write(input.data(), samples);
     std::cout << "finished generating\n";
     int it = 0;
 
     //cleans up data using hann window https://stackoverflow.com/questions/3555318/implement-hann-window
-    std::transform(input.begin(), input.end(), [&](double x){ return x * 0.5(1-std::cos((TAU * it++)/(input.size()-1)))})
+    // std::transform(input.begin(), input.end(), [&](double x){ return x * 0.5(1-std::cos((TAU * it++)/(input.size()-1)))});
     auto x = dft_para(input);
     for(size_t i = 0; i < x.size(); ++i) {
         if(abs(x[i].imag()) > 1 || abs(x[i].real()) > 1)
-            std::cout << input[i] << " : " << ((double)i * sf_info.samplerate() / samples)<< " : " << x[i] << '\n';
+            std::cout << input[i] << " : " << ((double)i * file.samplerate() / samples)<< " : " << x[i] << '\n';
     }
     for(auto i : input) {std::cout << i << ", ";}
 }
