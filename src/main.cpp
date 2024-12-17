2828b8add7b51a8cea444927cfc24208c94b2734
ImNutes <150874362+ImNutes@users.noreply.github.com>
2024-12-17 08:58:54 -0800

    threading

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,31 @@
 #include <cmath>
 #include <thread>
 
-#include <sndfile.h>
+#include <sndfile.hh>
 #include <fstream>
 #define PI 3.14159265358979323846
 #define TAU 2*PI
 
-std::complex<double> expr(double in, double freq, size_t size, int place) {
-    using namespace std::complex_literals;
-    return in * (std::exp(-1.0i * (TAU/size) * (double)place));
-}
-void dft(std::vector<double> input, size_t pos, std::vector<std::complex<double>>& res) {
+
+/* this is disgustingly unoptimized, i'm only doing this to showcase the properties of the discrete fourier transform.
+ * in any other program, i would use the fft and be done.
+*/
+void dft_range(std::vector<double> input, size_t pos, size_t range,std::vector<std::complex<double>>& res) {
+    for(size_t i = pos; i < range || i < input.size(); ++i) {
         for(size_t j = 0; j < input.size(); ++j) {
             std::complex<double> exponent = std::complex<double>(0.0, (-TAU * pos * j)/input.size());
             res[pos] += input[j] * std::exp(exponent);
         }
+    }
 }
 std::vector<std::complex<double>> dft_para(std::vector<double> input) {
-    std::vector<std::thread> threads;
+    const int THREAD_COUNT = 8;
+    int THREAD_SPACE = ceil(input.size()/THREAD_COUNT);
+    std::array<THREAD_COUNT> threads;
     std::vector<std::complex<double>> out(input.size());
-    for(size_t k = 0; k < input.size(); ++k) {
+    for(size_t k = 0; k < THREAD_COUNT; k += THREAD_SPACE) {
         std::cout << k << " of " << input.size() << '\n';
-        threads.push_back(std::thread(dft, std::ref(input), k,std::ref(out)));
+        threads[k] = std::thread(dft_range, k, (THREAD_SPACE), out);
     }
     for(auto& i : threads) {
         i.join();
@@ -35,13 +39,9 @@ std::vector<std::complex<double>> dft_para(std::vector<double> input) {
 }
 
 int main() {
-    SF_INFO sf_info;
-    SNDFILE* file = sf_open("audio.wav", SFM_READ, &sf_info);
-    if(file == nullptr) {
-        std::cout << "bad\n";
-        return -1;
-    } 
-    size_t samples = sf_info.frames;
+    SndfileHandle file;
+    file = SndfileHandle("audio.wav");
+    size_t samples = file.frames();
     std::vector<double> input(samples);
     std::vector<float> buf(samples);
     sf_readf_float(file, buf.data(), samples);
@@ -50,11 +50,14 @@ int main() {
     }
     sf_close(file);
     std::cout << "finished generating\n";
-    std::vector<double> y;
+    int it = 0;
+
+    //cleans up data using hann window https://stackoverflow.com/questions/3555318/implement-hann-window
+    std::transform(input.begin(), input.end(), [&](double x){ return x * 0.5(1-std::cos((TAU * it++)/(input.size()-1)))})
     auto x = dft_para(input);
     for(size_t i = 0; i < x.size(); ++i) {
         if(abs(x[i].imag()) > 1 || abs(x[i].real()) > 1)
-            std::cout << input[i] << " : " << ((double)i * sf_info.samplerate / samples)<< " : " << x[i] << '\n';
+            std::cout << input[i] << " : " << ((double)i * sf_info.samplerate() / samples)<< " : " << x[i] << '\n';
     }
     for(auto i : input) {std::cout << i << ", ";}
 }
