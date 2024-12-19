7da624bc23de95ea56e7a99d16dace93e63117a0
ImNutes <awesomeofdalek@gmail.com>
2024-12-18 17:29:48 -0800

    e

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,9 @@ int main() {
     int N = 4096; //size of a chunk
     int overlap = N/2;
     kiss_fftr_cfg cfg = kiss_fftr_alloc(N, 0, nullptr, nullptr);
+    
+    int chunk = 0;
+    std::vector<double> aggr_output(N/2);
     if(!cfg) { std::cout << "bad\n"; return -1;}
     for(size_t i = 0; i < samples; i += (N - overlap)) {
         for(size_t j = 0; j < N; ++j) {
@@ -38,10 +41,19 @@ int main() {
         }
         kiss_fftr(cfg, in, out);
         for(size_t p = 0; p < N/2; ++p) {
+            aggr_output[p] += std::sqrt( std::pow(out[p].r, 2) + std::pow(out[p].i, 2) );
             if(abs(out[p].r) > 100 || abs(out[p].i) > 100)
                 std::cout << "Bin "<< i << " : " << p << " of " << samples << " freq: " << (p * ((double)file.samplerate()/N)) << " : (" << out[p].r << ", " << out[p].i << ")" << '\n';
         }
+        chunk++;
     }
+    std::vector<double> freq(N/2);
+    for(size_t i = 0; i < N/2; ++i) {
+        aggr_output[i] /= (double)chunk;
+        freq[i] = i * (double)file.samplerate()/N;
+        std::cout << freq[i] << " : " << aggr_output[i] << '\n';
+    }
+    matplot::plot(freq, aggr_output);
     std::cout << "was stepping by " << (double)file.samplerate()/N << '\n';
     free(cfg);
     return 0;
