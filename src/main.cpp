90d03248e738871b2288c2898a099aa7bb0f244a
ImNutes <150874362+ImNutes@users.noreply.github.com>
2024-12-19 00:57:40 +0000

    things

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 #include <sndfile.hh>
 #include <fstream>
-#include "kiss_fft.h"
+#include "kiss_fftr.h"
 #define PI 3.14159265358979323846
 #define TAU 2*PI
 
@@ -17,21 +17,32 @@ int main() {
     SndfileHandle file;
     file = SndfileHandle("audio.wav");
     size_t samples = file.frames();
-    std::vector<double> input(samples);
-    kiss_fft_cpx in[samples];
+    std::vector<float> input(samples);
+    float in[samples];
     kiss_fft_cpx out[samples];
-    file.write(in.r, samples);
-    kiss_fft_cfg = kiss_fft_alloc(samples, 0, nullptr, nullptr);
+    file.read(input.data(), samples);
+    std::cout << "writing samples\n";
+    int N = 4096; //size of a chunk
+    int overlap = N/2;
+    kiss_fftr_cfg cfg = kiss_fftr_alloc(N, 0, nullptr, nullptr);
     if(!cfg) { std::cout << "bad\n"; return -1;}
-    kiss_fft(cfg, in, out);
-
-    std::cout << "finished copying\n";
-    int it = 0;
-
-    //cleans up data using hann window https://stackoverflow.com/questions/3555318/implement-hann-window
-    // std::transform(input.begin(), input.end(), [&](double x){ return x * 0.5(1-std::cos((TAU * it++)/(input.size()-1)))});
-    for(size_t i = 0; i < x.size(); ++i) {
-        std::cout << "Bin "<< i << "freq: " <<i * (file.samplerate()/samples) << " : (" << out[i].r << out[i].i << ")" << '\n';
+    for(size_t i = 0; i < samples; i += (N - overlap)) {
+        for(size_t j = 0; j < N; ++j) {
+            if(i + j < samples) {
+                double hann = 0.5 * (1.0 - cos(2 * M_PI * j / (N - 1))); //helps reduce spectral leakage 
+                in[j] = input[i + j] * hann;
+            } else {
+                //otherwise pad input
+                in[j] = 0;
+            }
+        }
+        kiss_fftr(cfg, in, out);
+        for(size_t p = 0; p < N/2; ++p) {
+            if(abs(out[p].r) > 100 || abs(out[p].i) > 100)
+                std::cout << "Bin "<< i << " : " << p << " of " << samples << " freq: " << (p * ((double)file.samplerate()/N)) << " : (" << out[p].r << ", " << out[p].i << ")" << '\n';
+        }
     }
+    std::cout << "was stepping by " << (double)file.samplerate()/N << '\n';
     free(cfg);
+    return 0;
 }
